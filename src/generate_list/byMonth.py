3d21d9e867b14a807a9034b35433d692f52fe93d
ivarejao <Igor Varejo>
2023-08-25 14:46:49 -0300

    Add get links by sector dividing by year

--- src/generate_list/byMonth.py
+++ src/generate_list/byMonth.py
@@ -5,24 +5,23 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from datetime import datetime
 import locale
-from time import sleep, strftime, gmtime
+from time import time, strftime, gmtime
 from pathlib import Path
 from argparse import ArgumentParser
 from tqdm import tqdm
-from typing import Dict
+from typing import Dict, List
 
 DATA_PATH = Path("/home/ivarejao/Neologism/data")
 
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument('--headless', help='Se habilitado o programa irá rodar sem gui', const='store_true', nargs='?')
-    parser.add_argument('--sector', help='Setor a ser listado', required=False, default='all')
+    parser.add_argument('--sector', help='Setor a ser listado', default='all')
     parser.add_argument('--time-range', help='Intervalo de tempo a ser procurado', default=[2008, 2023], nargs='+', type=int)
-    parser.add_argument('--by-month', help='Se habilitado os links serão salvos por mês', const='store_true', nargs='?')
 
     args = parser.parse_args()
     if args.time_range is not None and len(args.time_range) > 2:
-        raise ValueError("Para especificar o ano é necessário apenas informar 1 ou 2 anos")
+        raise ValueError("Para especificar o ano é necessário apenas informar 1 ano ou o intervalo eg. 2008,2023")
     return args
 
 def scroll_shim(passed_in_driver, object):
@@ -78,14 +77,14 @@ def clickButton(driver):
 
 def displayAllNews(driver):
     start_time = 0
-    start_function = sleep()
+    start_function = time()
     end_time = 0
     inner_count = 0
 
     convert_time = lambda x: strftime("%H:%M:%S", gmtime(x))
     while True and inner_count < 100: # end_time-start_time < 1200: 
         print(convert_time(end_time-start_time))
-        start_time = sleep()
+        start_time = time()
         # Try to get the list
         while True:
             try:
@@ -109,47 +108,66 @@ def displayAllNews(driver):
             
             inner_count += 1
 
-        end_time = sleep()
+        end_time = time()
 
     print(f"Last loop: {convert_time(end_time-start_time)}")
-    print(f"- Time to display: {convert_time(sleep()-start_function)}")
+    print(f"- Time to display: {convert_time(time()-start_function)}")
 
 
-def divide_links_by_month(driver, news_list) -> Dict:
+def divide_links_by_year(driver, news_list) -> Dict[int, List[str]]:
     # Iterate over each news and save it
     with tqdm(total=len(news_list), desc="Links: ", unit='l') as pb:
-        links_by_month = {}
+        links_by_year = {}
         for n in news_list:
             link = n.find_element(by=By.TAG_NAME, value="a").get_attribute("href")
-            # Get month
-            date_str = n.find_element(by=By.CLASS_NAME, value="author").find_element(by=By.XPATH, value="./span").text
-            result = date_str.split(" - ")
-            if len(result) == 1:
-                month = date_str.split(" ")[1]
+            # Get year
+            author_div = n.find_element(by=By.CLASS_NAME, value="author")
+            if "author blog-image" in author_div.get_attribute("class"):
+                date_str = author_div.text
+                year = date_str.split(",")[0].split(" ")[-1]
             else: 
-                month = result[1].split(" ")[3]
+                date_str = author_div.find_elements(by=By.TAG_NAME, value="span")[-1].text
+                result = date_str.split(",")
+                # There are two possibilities for date_str
+                # `Atualizado em 31 dez 2022, 18h04 - Publicado em 31 dez 2022, 18h00 ` or
+                # `31 dez 2022, 16h52 `
+                result = result[0] if len(result) == 2 else result[1]
+                year = result.split(" ")[-1]
          
             # Add link
-            if month in links_by_month.keys():
-                links_by_month[month].append(link)
+            if year in links_by_year.keys():
+                links_by_year[year].append(link)
             else:
                 # Initialize list
-                links_by_month[month] = [link]
+                links_by_year[year] = [link]
             pb.update()
 
-    return links_by_month
-
-def divide_links_by_year(driver, news_list, year : int) -> Dict:
-    # Iterate over each news and save it
-    with tqdm(total=len(news_list), desc="Links: ", unit='l') as pb:
-        links_by_year = {year : []}
-        for n in news_list:
-            link = n.find_element(by=By.TAG_NAME, value="a").get_attribute("href")
-         
-            # Add link
-            links_by_year[year].append(link)
-            pb.update()
     return links_by_year
+
+#def divide_links_by_year(driver, news_list, year : int) -> Dict:
+#    # Iterate over each news and save it
+#    with tqdm(total=len(news_list), desc="Links: ", unit='l') as pb:
+#        links_by_year = {year : []}
+#        for n in news_list:
+#            link = n.find_element(by=By.TAG_NAME, value="a").get_attribute("href")
+#         
+#            # Add link
+#            links_by_year[year].append(link)
+#            pb.update()
+#    return links_by_year
+
+def define_time_range(args):
+    
+    if len(args.time_range) > 1:
+        start = args.time_range[0]
+        end = args.time_range[1]
+        years = list(range(start, end+1))
+        years_str = f"[{start}, {end}]" # Just for log
+    else:
+        years = args.time_range
+        years_str = years[0]
+
+    return years, years_str
 
 
 def main():
@@ -161,16 +179,7 @@ def main():
     args = parse_args()
 
     sector = args.sector
-    BY_MONTH = args.by_month
-
-    if len(args.time_range) > 1:
-        start = args.time_range[0]
-        end = args.time_range[1]
-        years = list(range(start, end+1))
-        years_str = f"[{start}, {end}]" # Just for log
-    else:
-        years = args.time_range
-        years_str = years[0]
+    
 
     # Start session
     options = webdriver.FirefoxOptions()
@@ -183,15 +192,22 @@ def main():
         os.environ['MOZ_HEADLESS'] = '1'
     driver = webdriver.Firefox(options=options)
 
+
     print("Começando a coleta dos links de notícias")
     print(f"Setor: {sector} ")
-    print(f"Intervalo de tempo: {years_str}")
+    if sector == "all":
+        search_space, years_str = define_time_range(args)        
+        print(f"Intervalo de tempo: {years_str}")
+    else:
+        search_space = [sector]
     print("---")
 
-    for y in years:
-        driver.get(f"https://veja.abril.com.br/{y}/")
-        print(f"Year:{y}")
-
+    for element in search_space:
+        driver.get(f"https://veja.abril.com.br/{element}/")
+        print(("Year: " if sector == "all" else "Sector: ") + f"{element}")
+        #driver.execute_script("document.body.style.zoom='50%'")
+        driver.execute_script("document.body.style.MozTransform='scale(0.3  )';")
+        
         # Scroll down the page to show all news
         displayAllNews(driver)
 
@@ -199,14 +215,12 @@ def main():
         # List all news 
         element_news_list = driver.find_element(by=By.ID, value='infinite-list')
         news_list = element_news_list.find_elements(by=By.XPATH, value="//*[starts-with(@id, 'post')]")
-        LINKS_PATH = DATA_PATH / "all" / "links" / str(y)
+        
+        LINKS_PATH = DATA_PATH / sector / "links" 
         # Create directory where it will be stored
         os.makedirs(LINKS_PATH, exist_ok=True)
 
-        if BY_MONTH:
-            links = divide_links_by_month(driver, news_list)
-        else:  # The default, separate by Year
-            links = divide_links_by_year(driver, news_list, y)
+        links = divide_links_by_year(driver, news_list)
         # Save links
         print("Saving links")
         for key, links in links.items():
